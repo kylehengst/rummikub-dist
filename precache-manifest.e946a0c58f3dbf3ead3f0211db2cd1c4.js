9a5f842dffd1c0b715b1361abb149bd20e9176f0
Kyle Hengst <kyle.hengst@ipushpull.com>
2020-04-27 20:53:59 +0100

    build

---
+++ precache-manifest.e946a0c58f3dbf3ead3f0211db2cd1c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5fa6e19f094e9d91dfc",
+    "url": "/css/app.e7469fbf.css"
+  },
+  {
+    "revision": "fddc268afabefc8d15bf2e1a28213a95",
+    "url": "/img/joker.png"
+  },
+  {
+    "revision": "cfeb05a24fe7e0090c2ebd7bd61799ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5fa6e19f094e9d91dfc",
+    "url": "/js/app.64098d7f.js"
+  },
+  {
+    "revision": "5127807f73c1b0d0d07b",
+    "url": "/js/chunk-vendors.bbe00bbc.js"
+  },
+  {
+    "revision": "ff09cf59b229e093a14727364309b764",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
